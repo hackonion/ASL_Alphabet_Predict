acc33c85fdceac75397fafe39c772f69942b9e70
hackonion <jorge21913@hotmail.com>
2020-10-28 15:49:13 -0600

    Change epochs of 5 to 10 and Dropout of 0.25 to 0.5

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#Import library for used
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential
@@ -10,12 +11,10 @@ k.clear_session()
 
 data_training = '<Path of data for training>'
 
-epochs = 5
+epochs = 10
 size = (64,64)
 inp_shape = (64,64,3)
 bach_size = 64 
-steps = 1000
-validation_steps = 200
 filtrosConv1 = 32
 filtrosConv2 = 64
 filtrosConv3 = 128
@@ -58,12 +57,12 @@ cnn = Sequential()
 cnn.add(Convolution2D(filtrosConv1,size_filter1,padding='same',input_shape =inp_shape,activation='relu'))
 cnn.add(Convolution2D(filtrosConv1,size_filter1,padding='same',activation='relu'))
 cnn.add(MaxPool2D(pool_size = size_pool))
-cnn.add(Dropout(0.25))
+cnn.add(Dropout(0.5))
 
 cnn.add(Convolution2D(filtrosConv2,size_filter2,padding='same',activation='relu'))
 cnn.add(Convolution2D(filtrosConv2,size_filter2,padding='same',activation='relu'))
 cnn.add(MaxPool2D(pool_size = size_pool))
-cnn.add(Dropout(0.25))
+cnn.add(Dropout(0.5))
 
 cnn.add(Convolution2D(filtrosConv3,size_filter2,padding='same',activation='relu'))
 cnn.add(Convolution2D(filtrosConv3,size_filter2,padding='same',activation='relu'))
